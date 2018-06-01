a3f7ccc436d0a83c87500a009e250ffdf0674dad
Shakhor Smith <shakhorsmith@gmail.com>
2018-06-01 17:24:49 -0400

    Added server script

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
 const express = require('express')
 const app = express()
 
-app.get('', (req, res) => {
-    res.send('Server is working')
-})
+if (process.env.NODE_ENV === 'production') {
+  // Exprees will serve up production assets
+  app.use(express.static('client/build'));
+
+  // Express serve up index.html file if it doesn't recognize route
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
+// app.get('/', (req, res) => {
+//     res.send('Server is working')
+// })
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
