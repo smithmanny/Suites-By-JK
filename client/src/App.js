a855a8df592c11037e6a7be69a444705124d1131
Shakhor Smith <shakhorsmith@gmail.com>
2018-08-04 15:38:40 -0400

    Add coming soon page

--- client/src/App.js
+++ client/src/App.js
@@ -1,27 +1,30 @@
 import React, { Component } from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 // Assets
-import Landing from './components/Landing/Landing';
-import Header from './components/Header/Header';
 import About from './components/About/About';
-import Packages from './components/Packages/Packages';
 import Contact from './components/Contact/Contact';
+import ComingSoon from './components/ComingSoon/ComingSoon';
 import Footer from './components/Footer/Footer';
+import Header from './components/Header/Header';
+import Landing from './components/Landing/Landing';
+import Packages from './components/Packages/Packages';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <BrowserRouter>
-        <div className="container">
-          <Header />
+        {/* <div className="container"> */}
+        <div className="coming-soon">
+          {/* <Header /> */}
             <Switch>
-              <Route exact path="/" component={Landing} />
+              {/* <Route exact path="/" component={Landing} /> */}
+              <Route exact path="/" component={ComingSoon} />
               <Route path="/about" component={About} />
               <Route path="/packages" component={Packages} />
               <Route path="/contact" component={Contact} />
             </Switch>
-          <Footer />
+          {/* <Footer /> */}
         </div>
       </BrowserRouter>
     );
---
+++ client/src/components/ComingSoon/ComingSoon.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+import Newsletter from '../Newsletter/Newsletter';
+
+ class ComingSoon extends Component {
+  render() {
+    return (
+      <div>
+        <h3>Be Ready, We Are Lanching Soon.</h3>
+        <h1 className="company-name">Suites By JK</h1>
+        <Newsletter text='' />
+      </div>
+    )
+  }
+}
+
+export default ComingSoon;
