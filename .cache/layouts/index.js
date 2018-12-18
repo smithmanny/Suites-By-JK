13538de1e3d05e5ceb52a495a0d4931381ce7866
Shakhor Smith <shakhorsmith@gmail.com>
2018-12-17 20:41:17 -0500

    clear cache

--- .cache/layouts/index.js
+++
@@ -1,7 +0,0 @@
-
-  import React from "react"
-  import Component from "/Users/shakhor/Projects/Web/Suites-By-JK/src/layouts/index.js"
-  import data from "/Users/shakhor/Projects/Web/Suites-By-JK/.cache/json/layout-index.json"
-
-  export default (props) => <Component {...props} {...data} />
-  
